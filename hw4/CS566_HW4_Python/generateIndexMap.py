1c3fb1c4e58e2d0d45d8f15dca1ad85b70f3abfd
matejpopovski <matej.popovski@gmail.com>
2025-11-12 01:50:16 -0600

    hw4

--- hw4/CS566_HW4_Python/generateIndexMap.py
+++ hw4/CS566_HW4_Python/generateIndexMap.py
@@ -3,37 +3,38 @@ from scipy.signal import convolve2d
 from skimage import img_as_float
 
 
+def _box_filter(img, r):
+    if r <= 0:
+        return img
+    k = np.ones((2*r+1, 2*r+1), dtype=np.float32) / ((2*r+1)*(2*r+1))
+    return convolve2d(img, k, mode="same", boundary="symm")
+
+
 def generate_index_map(gray_stack, w_size):
+    """
+    Compute index map via Sum-Modified Laplacian (SML) focus measure.
+    gray_stack: (H, W, N) float array in [0,1]
+    w_size: half window size for spatial averaging of the SML (typ. 8~16)
+    Returns:
+        index_map: (H, W) int32 with best-focused layer index (0..N-1)
+    """
     H, W, N = gray_stack.shape
+    gray_stack = img_as_float(gray_stack)
 
-    # Compute the focus measure -- the sum-modified laplacian
-    #
-    # horizontal Laplacian kernel
+    # Modified Laplacian kernels
     Kx = np.array([[0.25, 0, 0.25],
-                   [1,   -3,  1],
-                   [0.25, 0, 0.25]])
-    Ky = Kx.T   # vertical version
+                   [1.0,  -3.0, 1.0],
+                   [0.25, 0, 0.25]], dtype=np.float32)
+    Ky = Kx.T
 
-    # horizontal and vertical Laplacian responses
-    Lx = np.zeros((H, W, N))
-    Ly = np.zeros((H, W, N))
-    for n in range(N):
-        I = img_as_float(gray_stack[:, :, n])
-        Lx[:, :, n] = convolve2d(I, Kx, mode="same", boundary="symm")
-        Ly[:, :, n] = convolve2d(I, Ky, mode="same", boundary="symm")
+    # Focus measure volume (H, W, N)
+    F = np.zeros((H, W, N), dtype=np.float32)
+    for i in range(N):
+        I = gray_stack[:, :, i]
+        Lx = convolve2d(I, Kx, mode='same', boundary='symm')
+        Ly = convolve2d(I, Ky, mode='same', boundary='symm')
+        SML = np.abs(Lx) + np.abs(Ly)              # focus measure
+        F[:, :, i] = _box_filter(SML, w_size)      # spatial smoothing
 
-    # sum-modified Laplacian
-    SML = (np.abs(Lx) ** 2) + (np.abs(Ly) ** 2)
-    # can also use the absolute value itself
-    # this is probably more well-known
-    # SML = np.abs(Lx) + np.abs(Ly)
-
-    ###########################################################################
-    # ADD YOUR CODE HERE
-    ###########################################################################
-
-    ###########################################################################
-    # TODO: replace with your index_map
-    index_map = np.zeros(
-        (gray_stack.shape[0], gray_stack.shape[1]), dtype=np.int32)
+    index_map = np.argmax(F, axis=2).astype(np.int32)
     return index_map
